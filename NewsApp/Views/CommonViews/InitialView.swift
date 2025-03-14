6a8c5bffe47ea006ccd60864934e9e105bd0437a
Alex Chebotarov <imoleksiy@gmail.com>
2025-03-14 16:52:13 +0000

    - initial comit

---
+++ NewsApp/Views/CommonViews/InitialView.swift
@@ -0,0 +1,26 @@
+//
+//  InitialView.swift
+//  NewsApp
+//
+//  Created by Oleksiy Chebotarov on 14/03/2025.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    @Environment(\.api) private var api
+    let retryAction: () async -> Void
+
+    var body: some View {
+        VStack {
+            Text("No data loaded")
+                .foregroundColor(.red)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            RetryButton {
+                await retryAction()
+            }
+        }
+    }
+}
